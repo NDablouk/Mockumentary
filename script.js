9cdc0e4d0be1a699d9a7e287995c600ce25aefac
Nibras Dablouk <384422@LG203MAC10.local>
2024-03-04 15:21:00 +0000

    added in small game

---
+++ script.js
@@ -0,0 +1,59 @@
+alert("welcome to my game")
+
+var boringanswer = confirm("do you find washing dishes boring? yes or no")
+if (boringanswer === false) {
+    var explanation =prompt("explain how you would make dishwashing more fun and manageable")
+    alert("this is cool:" + explanation)
+    var music = confirm("do you listen to music while dishwashing? yes or no")
+    if(music === true){
+       var song = prompt("what is your favorite song.")
+    alert("my new favorite song"+ song)
+    var dance = confirm("Do you dance with the rythm of the wash?")
+if(dance === true){
+    alert("GREAT!!")
+}
+else{
+    alert("it would be great if you would lighten things up a bit and have more fun!")
+}
+    alert("Thank you for your response!")
+    }else{
+        alert("maybe try listening to mozart")
+        alert("Thank you for your response!")
+    }
+}else{
+    alert("So sorry I do not agree!")
+}
+
+
+
+
+
+// if(boringanswer === true)
+// var nauseousanswer = confirm("do feel nauseous while washing dishes? yes or no")
+// if(nauseousanswer === false) confirm("do you listen to music while dishwashing? yes or no")
+// else(nauseousanswer === true) = prompt("what emotions do you expericence while washing dishes?") 
+// var musicanswer = confirm("do you listen to music while dishwashing? yes or no")
+// if(musicanswer === false) prompt("explain how you would make dishwashing more fun and manageable")
+// else (musicanswer) = alert(explanation)
+
+
+
+
+
+
+
+
+// alert("Thank you for your response!")
+// prompt("describe how much memory loss you have experienced due to dish washing?")
+// confirm("do you wash dishes everyday? yes or no")
+// confirm("do you have a dishwasher? yes or no")
+// prompt("explain how you would make dishwashing more fun and manageable")
+// confirm("do you listen to music while dishwashing? yes or no")
+// confirm("do you put on hand cream after washing dishes")
+// confirm("do wear washing gloves")
+// prompt("how many hours do you approxiamtely spend washing dishes per week?")
+// prompt("what usually goes through your head while washing dishes?")
+// confirm("do feel nauseous while washing dishes?")
+// confirm("do you feel dizzy while washing dishes?")
+// confirm("Are there anyone who can help with dishwashing at home?")
+// prompt("what emotions do you expericence while washing dishes?")
